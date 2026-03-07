b3dd064bff794df9a4f42f265639add5cc4adc92
paulfrancisyramu-cmd <paulfrancisyramu@gmail.com>
2026-03-08 04:06:33 +0800

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -235,10 +235,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 });
-
-const menuBtn = document.getElementById('menu-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-menuBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
